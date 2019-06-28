296837df14b874d6fff687a8281088ef07042e51
ArabaEmile <arabaemilek@gmail.com>
2019-06-28 06:32:54 +0200

    Setup and add static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       
+        <title>Laravel</title>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+         <style>
+            footer{
+                margin: 4em 0;
+            }
+        </style>
+   </head>
+    <body>
+       <div class="flex-center position-ref full-height">
+           
+        </div>
+        @include('layouts/partials/_nav')
+          @yield('content')
+        @include('layouts/partials/_footer')
+        
+        
+       
+        <script src="//code.jquery.com/jquery.min.js"></script>
+        <!-- Optional theme -->
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.default')
+@section('content')
+    <div clas="container">
+        <h2>What is Laracarte?</h2>
+        <p>Laracarte is a clone app of <a href="https://laramap.com" target="_blank"> Laramap.com.</a></p>
+
+        <div class="row">
+            <div class="col-md-6">
+                <p class="alert alert-warning">
+                    <strong><i class="fas fa-exclamation-triangle"></i>This app has been built by @etsmo for learning purposes.</strong>
+                </p>
+            </div>
+        </div>
+        <p>Feel free to help to improve the source code.</p>
+        <hr>
+        <h2>What is Laramap?</h2>
+        <p>Laramap is the website by which Laracarte was inspired :).</p>
+        <p>More info here  <a href="https://laramap.com/p/about"></a>.</p>
+        <hr>
+        <h2>Which tools and services are used in Laracarte?</h2>
+        <p>Basically it's built on Laravel & Bootstrap. But there's a bunch of services used for email and other sections.Laravel</p>
+        <ul>
+        <li>Laravel</li>
+        <li>Bootstrap</li>
+        <li>Amazon S3</li>
+        <li>Mandrill</li>
+        <li>Google Maps</li>
+        <li>Gravatars</li>
+        <li>Antony Martin's Geolocation Package</li>
+        <li>Michel Fortin's Markdown Parser Package</li>
+        <li>Heroku</li>
+        </ul>
+    </div>
+@endsection

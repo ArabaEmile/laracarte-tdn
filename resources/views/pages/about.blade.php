3e0d7f06d4d6612d362ae145868f86622cc3d4cc
ArabaEmile <arabaemilek@gmail.com>
2019-06-29 07:56:03 +0200

    config_laracarte

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.default',['title'=>'About'])
 @section('content')
     <div clas="container">
-        <h2>What is Laracarte?</h2>
-        <p>Laracarte is a clone app of <a href="https://laramap.com" target="_blank"> Laramap.com.</a></p>
+        <h2>What is {{config('app.name')}}?</h2>
+        <p>{{config('app.name')}} is a clone app of <a href="https://laramap.com" target="_blank"> Laramap.com.</a></p>
 
         <div class="row">
             <div class="col-md-6">
@@ -14,7 +14,7 @@
         <p>Feel free to help to improve the <a href="https://github.com/ArabaEmile/laracarte-tdn"> source code</a>.</p>
         <hr>
         <h2>What is Laramap?</h2>
-        <p>Laramap is the website by which Laracarte was inspired :).</p>
+        <p>Laramap is the website by which {{config('app.name')}} was inspired :).</p>
         <p>More info here  <a href="https://laramap.com/p/about"></a>.</p>
         <hr>
         <h2>Which tools and services are used in Laracarte?</h2>

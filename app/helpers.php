b608a6ce8910557602ad3d5561a751ae1115ec52
ArabaEmile <arabaemilek@gmail.com>
2019-06-29 06:25:39 +0200

    set_active_route

--- app/helpers.php
+++ app/helpers.php
@@ -9,3 +9,12 @@
      return $title . '|' . $base_title;
     }
  }
+
+
+ if(!function_exists('set_active_route')){
+    
+     function set_active_route($route){
+        return Route::is($route)? 'active' : '';
+      
+  }
+}
